b699d5ef12b5f04e6c6307ef3d0f2aeaafab42a8
Huzaifa Elahi <huzaifaelahi1@yahoo.com>
2018-11-04 17:27:24 -0500

    Comments Added
    Commented code to explain algorithm for DFS and Ford-Fulkerson

--- A3/FordFulkerson.java
+++ A3/FordFulkerson.java
@@ -10,16 +10,20 @@ public class FordFulkerson {
 
 
 	public static ArrayList<Integer> pathDFS(Integer source, Integer destination, WGraph graph){
+
+		// Stack : Stack of nodes in the DFS path
+		// Forbidden: List of nodes visited
+		// start: Node to travel from for specific edge
 		ArrayList<Integer> Stack = new ArrayList<Integer>();
-
-		Stack.add(source);
 		ArrayList<Integer> forbidden = new ArrayList<Integer>();
-
+		Stack.add(source);
 		int start = source;
 		boolean foundEdge = false;
 		forbidden.add(source);
 
 		while(true) {
+
+			// Brute force search for first adjacent node 
 			for(int i = 0; i <= destination ; i++) {
 				if(graph.getEdge(start, i) != null && !forbidden.contains(i) && graph.getEdge(start, i).weight > 0) {
 					Stack.add(i);
@@ -29,13 +33,18 @@ public class FordFulkerson {
 					break;
 				}
 			}
+
+			// If no edge found; return if stack is 1 else remove top of Stack
 			if(!foundEdge) {
 				if(Stack.size() == 1) {
 					return null;
+				}else {
+					Stack.remove(Stack.size()-1);
+					start = Stack.get(Stack.size()-1);
 				}
-				Stack.remove(Stack.size()-1);
-				start = Stack.get(Stack.size()-1);
 			}
+			
+			// If not at end: continue, else exit
 			if(start != destination) {
 				foundEdge = false;
 				continue;
@@ -53,12 +62,8 @@ public class FordFulkerson {
 		String myMcGillID = "260726386"; //Please initialize this variable with your McGill ID
 		int maxFlow = 0;
 
-		WGraph graphF = new WGraph(graph);
-		int bottleNeck = 99999;
-
-		ArrayList<Edge> edgesCapabilities = graph.getEdges();
-
 		// Set edge flows to 0
+		WGraph graphF = new WGraph(graph);
 		ArrayList<Edge> edgesGraphF = graphF.getEdges();
 		for(Edge e:edgesGraphF) {
 			e.weight = 0;
@@ -72,6 +77,8 @@ public class FordFulkerson {
 		}
 
 		// Find bottleneck
+		int bottleNeck = 99999;
+		ArrayList<Edge> edgesCapabilities = graph.getEdges();
 		for(Edge e : edgesCapabilities) {
 			bottleNeck = Math.min(bottleNeck, e.weight);
 		}
@@ -82,10 +89,8 @@ public class FordFulkerson {
 			edge.weight = bottleNeck;
 		}
 
-
-
 		while(true) {
-			
+
 			edgesGraphF = graphF.getEdges();
 			ArrayList<Edge> forward = new ArrayList<Edge>();
 			ArrayList<Edge> backward = new ArrayList<Edge>();
@@ -93,11 +98,15 @@ public class FordFulkerson {
 			// Create residual graph
 			WGraph residue = new WGraph();
 			for(Edge e : edgesGraphF) {
+
+				// Forward edge
 				if(e.weight < graph.getEdge(e.nodes[0], e.nodes[1]).weight) {
 					Edge forwardEdge = new Edge(e.nodes[0], e.nodes[1],(graph.getEdge(e.nodes[0], e.nodes[1]).weight - e.weight));
 					residue.addEdge(forwardEdge);
 					forward.add(forwardEdge);
 				}
+
+				// Backward edge
 				if(e.weight > 0) {
 					Edge backwardEdge = new Edge(e.nodes[1], e.nodes[0],(e.weight));
 					residue.addEdge(backwardEdge);
@@ -107,22 +116,20 @@ public class FordFulkerson {
 
 			// Check if path from s to t exists in residual graph
 			ArrayList<Integer> nodesResidual = pathDFS(source, destination, residue);
-		
-			// If no path found
+
+			// If no path found, set graph and break
 			if(nodesResidual == null) {
-				System.out.println(-1);
 				graph=graphF;
 				break;
 			}
 
+			// Find bottleneck
 			ArrayList<Edge> residueEdges = residue.getEdges();
 			bottleNeck = 9999;
-		
-			// Find bottleneck
 			for(Edge e : residueEdges) {
 				bottleNeck = Math.min(bottleNeck, e.weight);
 			}
-			
+
 			// Augment the Path
 			for(int i = 0 ; i < nodesResidual.size()-1 ; i++) {
 				Edge edge = residue.getEdge(nodesResidual.get(i), nodesResidual.get(i+1)) ;
@@ -132,19 +139,25 @@ public class FordFulkerson {
 			// Add to G
 			for(int i = 0 ; i < nodesResidual.size()-1 ; i++) {
 				Edge e = residue.getEdge(nodesResidual.get(i), nodesResidual.get(i+1)) ;
-				if(forward.contains(e)) {
+
+				// Forward Edge
+				if(forward.contains(e)) {    
 					graphF.getEdge(e.nodes[0], e.nodes[1]).weight += e.weight ;
-				} else if(backward.contains(e)) {
+
+					// Backward edge
+				} else if(backward.contains(e)) {  
 					graphF.getEdge(e.nodes[1], e.nodes[0]).weight -= e.weight ;
 				}
 			}
+
+			// Find maxFlow : flow out of source
 			maxFlow = 0;
 			for(int i =0; i < destination; i++) {
 				if(graphF.getEdge(0, i)!= null) {
 					maxFlow+=graphF.getEdge(0, i).weight;
 				}
 			}
-			
+
 		}
 		answer += maxFlow + "\n" + graph.toString();	
 		writeAnswer(filePath+myMcGillID+".txt",answer);
